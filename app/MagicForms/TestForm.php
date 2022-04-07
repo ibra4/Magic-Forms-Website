643067e2dd808222958198d735e0b8f1aa3bb7f5
ibra <i.hammad@syarah.com>
2022-04-08 00:59:52 +0300

    Update Magic Forms

--- app/MagicForms/TestForm.php
+++ app/MagicForms/TestForm.php
@@ -4,15 +4,30 @@ namespace App\MagicForms;
 
 use Ibra\MagicForms\Fields\TextField;
 use Ibra\MagicForms\Builder\Form\MagicForm;
+use Ibra\MagicForms\Fields\NumberField;
+use Ibra\MagicForms\Fields\SelectField;
 use stdClass;
 
 class TestForm extends MagicForm
 {
+    public $id = "hahaha";
+
     public function build(): self
     {
         $user = new stdClass();
         $user->name = "user name";
         $this->model = $user;
+
+        $this->add(SelectField::class, [
+            'id' => 'hi1',
+            'name' => 'names',
+        ], [
+            'options' => [
+                '1' => 'One',
+                '2' => 'Two',
+                '3' => 'Three',
+            ]
+        ]);
 
         $this->add(TextField::class, [
             'id' => 'hi',
@@ -30,16 +45,19 @@ class TestForm extends MagicForm
             'name' => 'email',
         ]);
 
-        $this->action = route('test.store');
-
-        $this->method = "POST";
-
-        $this->ajax = false;
-        $this->id = "hi";
+        $this->add(NumberField::class, [
+            'name' => 'number_field',
+            'min' => 5,
+            'max' => 20
+        ]);
 
         return $this;
     }
 
+    public function action() {
+        return route('test.store');
+    }
+    
     public function rules(): array
     {
         return [
